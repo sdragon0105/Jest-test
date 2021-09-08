efbc1b047c536581c0c075087fb61e9655038046
sdragon0105 <77851434+sdragon0105@users.noreply.github.com>
2021-09-08 16:59:32 +0100

    Common Matchers test

--- fn.test.js
+++ fn.test.js
@@ -1,7 +1,8 @@
+const { TestWatcher } = require('jest');
 const fn = require('./fn');
 
 test("name and age", () => {
-    expect(fn.makeUser("Mike", 30)).toBe({
+    expect(fn.makeUser("Mike", 30)).toStrictEqual({
         name: "Mike",
         age: 30,
     });
@@ -13,3 +14,12 @@ test("name and age", () => {
         age: 30,
     });
 });
+
+// test("0 is false.", () => {
+//     expect(fn.add(1, -1)).toBeFalsy();
+// });
+
+// test("0 is false.", () => {
+//     expect(fn.add("Hello", "World!")).toBeTruthy();
+// });
+
