1c68023f358f7aa55208b85405c71d9698a59eb6
sdragon0105 <77851434+sdragon0105@users.noreply.github.com>
2021-09-08 17:30:03 +0100

    Truthiness Matcher test

--- fn.js
+++ fn.js
@@ -1,6 +1,6 @@
 const fn = {
     add: (num1, num2) => num1 + num2,
-    makeUser: (name, age) => ({name, age}),
+    makeUser: (name, age) => ({ name, age, gender: undefined }),
 };
 
 module.exports = fn;
--- fn.test.js
+++ fn.test.js
@@ -1,25 +1,25 @@
 const { TestWatcher } = require('jest');
 const fn = require('./fn');
 
-test("name and age", () => {
-    expect(fn.makeUser("Mike", 30)).toStrictEqual({
-        name: "Mike",
-        age: 30,
-    });
-});
-
-test("name and age", () => {
-    expect(fn.makeUser("Mike", 30)).toEqual({
-        name: "Mike",
-        age: 30,
-    });
-});
-
-// test("0 is false.", () => {
-//     expect(fn.add(1, -1)).toBeFalsy();
+// test("name and age", () => {
+//     expect(fn.makeUser("Mike", 30)).toStrictEqual({
+//         name: "Mike",
+//         age: 30,
+//     });
 // });
 
-// test("0 is false.", () => {
-//     expect(fn.add("Hello", "World!")).toBeTruthy();
+// test("name and age", () => {
+//     expect(fn.makeUser("Mike", 30)).toEqual({
+//         name: "Mike",
+//         age: 30,
+//     });
 // });
+
+test("0 is false.", () => {
+    expect(fn.add(1, -1)).toBeFalsy();
+});
+
+test("0 is false.", () => {
+    expect(fn.add("Hello", "World!")).toBeTruthy();
+});
 
