66289392a3b16cdfd812cb36c482a060f4cdb3e5
sdragon0105 <77851434+sdragon0105@users.noreply.github.com>
2021-09-08 16:05:04 +0100

    project upgrade

--- fn.test.js
+++ fn.test.js
@@ -1,7 +1,7 @@
 const fn = require('./fn');
 
 test("name and age", () => {
-    expect(fn.makeUser("Mike", 30)).toStrictEqual({
+    expect(fn.makeUser("Mike", 30)).toBe({
         name: "Mike",
         age: 30,
     });
